db0c6c82f9e4d872bd71d8a2bdcdfd54d9427aa1
Georgy Ivanov <ivanov.georg@gmail.com>
2016-03-05 10:07:33 +0300

    iniatial commit: add compilable version

---
+++ a_b.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main() {
+  short a;
+  short b;
+  scanf("%hd", &a);
+  scanf("%hd", &b);
+  printf("%hd\n", a + b);
+
+  int *data = new int[1000*1000*1000];
+
+  return 0;	
+}
+
